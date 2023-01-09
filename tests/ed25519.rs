5b897935e34da9dc2b02a4a794d935919bbe5e84
pinkforest <36498018+pinkforest@users.noreply.github.com>
2023-01-09 18:30:17 +1100

    Make rand_core optional

--- tests/ed25519.rs
+++ tests/ed25519.rs
@@ -16,9 +16,6 @@ use ed25519_dalek::*;
 use hex::FromHex;
 use hex_literal::hex;
 
-#[cfg(feature = "rand")]
-use sha2::Sha512;
-
 #[cfg(test)]
 mod vectors {
     use super::*;
@@ -281,10 +278,11 @@ mod vectors {
     }
 }
 
-#[cfg(feature = "rand")]
+#[cfg(feature = "rand_core")]
 mod integrations {
     use super::*;
     use rand::rngs::OsRng;
+    use sha2::Sha512;
 
     #[test]
     fn sign_verify() {
