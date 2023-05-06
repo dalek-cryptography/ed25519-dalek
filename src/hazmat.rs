7bbb07632e4892aea1f73ce641442e3711faa35c
Michael Rosenberg <michael@mrosenberg.pub>
2023-05-06 02:00:44 -0400

    Brought back ExpandedSecretKey; made raw_* functions take it as input

--- src/hazmat.rs
+++ src/hazmat.rs
@@ -10,53 +10,97 @@
 
 use crate::{
     signing::{private_raw_sign, private_raw_sign_prehashed},
-    Signature, SignatureError, VerifyingKey,
+    InternalError, Signature, SignatureError, VerifyingKey,
 };
 
 use curve25519_dalek::{
     digest::{generic_array::typenum::U64, Digest},
     Scalar,
 };
+use zeroize::Zeroize;
 
-/// Computes an ordinary Ed25519 signature over the given message.
+/// Contains the secret scalar and domain separator used for generating signatures.
+///
+/// In the usual Ed25519 signing algorithm, `scalar` and `hash_prefix` are defined such that
+/// `scalar || hash_prefix = H(sk)` where `sk` is the signing key and `H` is SHA-512.
+/// **WARNING:** Deriving the values for these fields in any other way can lead to full key
+/// recovery, as documented in [`raw_sign`] and [`raw_sign_prehashed`].
+///
+/// Instances of this secret are automatically overwritten with zeroes when they fall out of scope.
+pub struct ExpandedSecretKey {
+    /// The secret scalar used for signing
+    pub scalar: Scalar,
+    /// The domain separator used when hashing the message to generate the pseudorandom `r` value
+    pub hash_prefix: [u8; 32],
+}
+
+impl Drop for ExpandedSecretKey {
+    fn drop(&mut self) {
+        self.scalar.zeroize();
+        self.hash_prefix.zeroize()
+    }
+}
+
+impl ExpandedSecretKey {
+    /// Convert this `ExpandedSecretKey` into an array of 64 bytes.
+    pub fn to_bytes(&self) -> [u8; 64] {
+        let mut bytes: [u8; 64] = [0u8; 64];
+
+        bytes[..32].copy_from_slice(self.scalar.as_bytes());
+        bytes[32..].copy_from_slice(&self.hash_prefix[..]);
+        bytes
+    }
+
+    /// Construct an `ExpandedSecretKey` from a slice of bytes.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` whose okay value is an EdDSA `ExpandedSecretKey` or whose error value is an
+    /// `SignatureError` describing the error that occurred.
+    pub fn from_bytes(bytes: &[u8]) -> Result<ExpandedSecretKey, SignatureError> {
+        if bytes.len() != 64 {
+            return Err(InternalError::BytesLength {
+                name: "ExpandedSecretKey",
+                length: 64,
+            }
+            .into());
+        }
+        let mut lower: [u8; 32] = [0u8; 32];
+        let mut upper: [u8; 32] = [0u8; 32];
+
+        lower.copy_from_slice(&bytes[00..32]);
+        upper.copy_from_slice(&bytes[32..64]);
+
+        Ok(ExpandedSecretKey {
+            scalar: Scalar::from_bytes_mod_order(lower),
+            hash_prefix: upper,
+        })
+    }
+}
+
+/// Compute an ordinary Ed25519 signature over the given message.
 ///
 /// # ⚠️  Unsafe
 ///
-/// Do NOT use this function unless you absolutely must. Misuse of this function can expose your
-/// private key. See [here](https://github.com/MystenLabs/ed25519-unsafe-libs) for more details on
-/// this attack.
-///
-/// # Inputs
-///
-/// * `scalar` is the secret scalar of the signing key
-/// * `hash_prefix` is the domain separator that, along with the message itself, is used to
-///   deterministically generate the `R` part of the signature.
-///
-/// `scalar` and `hash_prefix` are usually selected such that `scalar || hash_prefix = H(sk)` where
-/// `sk` is the signing key
-pub fn raw_sign(
-    scalar: Scalar,
-    hash_prefix: [u8; 32],
-    message: &[u8],
-    verifying_key: &VerifyingKey,
-) -> Signature {
-    private_raw_sign(scalar, hash_prefix, message, verifying_key)
+/// Do NOT use this function unless you absolutely must. Using the wrong values in
+/// `ExpandedSecretKey` can leak your signing key. See
+/// [here](https://github.com/MystenLabs/ed25519-unsafe-libs) for more details on this attack.
+pub fn raw_sign(sk: ExpandedSecretKey, message: &[u8], verifying_key: &VerifyingKey) -> Signature {
+    private_raw_sign(sk.scalar, sk.hash_prefix, message, verifying_key)
 }
 
-/// Computes a signature over the given prehashed message, the Ed25519ph algorithm defined in
+/// Compute a signature over the given prehashed message, the Ed25519ph algorithm defined in
 /// [RFC8032 §5.1][rfc8032].
 ///
 /// # ⚠️  Unsafe
 //
-/// Do NOT use this function unless you absolutely must. Misuse of this function can expose your
-/// private key. See [here](https://github.com/MystenLabs/ed25519-unsafe-libs) for more details on
-/// this attack.
+/// Do NOT use this function unless you absolutely must. Using the wrong values in
+/// `ExpandedSecretKey` can leak your signing key. See
+/// [here](https://github.com/MystenLabs/ed25519-unsafe-libs) for more details on this attack.
 ///
 /// # Inputs
 ///
-/// * `scalar` is the secret scalar of the signing key
-/// * `hash_prefix` is the domain separator that, along with the prehashed message, is used to
-///   deterministically generate the `R` part of the signature.
+/// * `sk` is the [`ExpandedSecretKey`] being used for signing
 /// * `prehashed_message` is an instantiated hash digest with 512-bits of
 ///   output which has had the message to be signed previously fed into its
 ///   state.
@@ -78,8 +122,7 @@ pub fn raw_sign(
 #[cfg(feature = "digest")]
 #[allow(non_snake_case)]
 pub fn raw_sign_prehashed<'a, D>(
-    scalar: Scalar,
-    hash_prefix: [u8; 32],
+    sk: ExpandedSecretKey,
     prehashed_message: D,
     verifying_key: &VerifyingKey,
     context: Option<&'a [u8]>,
@@ -88,8 +131,8 @@ where
     D: Digest<OutputSize = U64>,
 {
     private_raw_sign_prehashed(
-        scalar,
-        hash_prefix,
+        sk.scalar,
+        sk.hash_prefix,
         prehashed_message,
         verifying_key,
         context,
